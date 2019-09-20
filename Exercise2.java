f5c1b1ffb24db6f6dfb660f32c917402d879e2ed
JODonovanKelly <55540624+JODonovanKelly@users.noreply.github.com>
2019-09-20 12:27:41 +0100

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)Exercise2.java
+ * @Jonathan O'Donovan Kelly 
+ * @version 1.00 2019/9/20
+ */
+
+
+public class Exercise2 {
+
+    public static void main(String args[]) {
+    	int pages = 317, year = 1999;
+    	String author = "J.K. Rowling", title = "Harry Potter and the Prisoner of Azkaban";
+    	Float price = 35.50f;
+    		
+    	System.out.println("Title: "+title+"\nAuthor: "+author+"\nNumber of pages: "+pages+"\nYear of Publication: "+year+"\nPrice: EUR"+price);
+    }
+    
+    
+}
