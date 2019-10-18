ec89ec8c3446967e70ec8eead01758804a1df3fb
JODonovanKelly <55540624+JODonovanKelly@users.noreply.github.com>
2019-10-18 10:47:33 +0100

    Add files via upload

---
+++ OldExercise2.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)Exercise2.java
+ * @Jonathan O'Donovan Kelly 
+ * @version 1.00 2019/9/20
+ */
+
+
+public class Exercise2 {
+
+    public static void main(String args[]) {
+    	int pages = 317, year = 1999;
+    	String author = "J.K. Rowling", title = "Harry Potter and the Prisoner of Azkaban", name = "Author:", subject = "Title:", sheets = "Pages:", date = "Year:", cost = "Price:", eur = "EUR";
+    	float price = 35.50f;
+    	
+    	String formatString = String.format("%-10s%-1s\n%-10s%-1s\n%-10s%-1d\n%-10s%-1d\n%-10s%-3s%.2f", subject,title,name,author,sheets,pages,date,year,cost,eur,price);
+    		
+    	System.out.println(formatString) ;
+    }
+    
+    
+}
